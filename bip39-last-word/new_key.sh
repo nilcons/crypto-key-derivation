f2d9d5b6db1462de7a0279fd62aee11383973043
Gergely Risko <errge@nilcons.com>
2018-04-18 15:25:16 +0200

    Add a simple key gen script

---
+++ bip39-last-word/new_key.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+set -e
+
+WORDS=$(gpg --gen-random 2 46 | od -An -tu2 -w2 | while read RET ; do
+	cat bip39-last-word/english.txt | sed -n "$(echo $((RET % 2048)))p"
+done | tr '\n' ' ')
+LAST=$(echo $WORDS | bip39-last-word/last_word.py | tail -n1)
+echo $WORDS $LAST
