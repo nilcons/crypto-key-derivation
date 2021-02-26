e1f6826af09d163d9f060e682610784a689471e8
Gergely Risko <errge@nilcons.com>
2021-02-26 21:27:05 +0100

    emulators/ledger-nano-s: remove no longer needed ledger-live dep

--- emulators/ledger-nano-s/00-download.sh
+++ emulators/ledger-nano-s/00-download.sh
@@ -7,5 +7,3 @@ mkdir -p down
 curl -o down/proxy.py https://raw.githubusercontent.com/LedgerHQ/speculos/master/tools/ledger-live-http-proxy.py
 curl -o down/ledger-live -L https://download-live.ledger.com/releases/latest/download/linux
 chmod a+x down/*
-npm install ledger-live
-rm -f package-lock.json package.json
