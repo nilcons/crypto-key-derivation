e3af719d687c2483ab4268a78ba2e723ea1409fa
Gergely Risko <errge@nilcons.com>
2021-03-01 15:41:51 +0100

    ledger emulator: better formatting on address output

--- emulators/ledger-nano-s/02-get-addresses.sh
+++ emulators/ledger-nano-s/02-get-addresses.sh
@@ -3,5 +3,9 @@
 set -e
 
 for c in btc eth xrp xlm ltc xtz; do
-    docker run -it --rm speculos-ledger-cli $c
+    docker run -it --rm speculos-ledger-cli $c \
+        | grep -v "Failed to initialize libusb" \
+        | grep -v "UTXOs" \
+        | cut -d: -f2- \
+        | cut -d')' -f1-2
 done
