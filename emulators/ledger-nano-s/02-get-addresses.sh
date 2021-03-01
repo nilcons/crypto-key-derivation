dd0a24755d041ede02e99ca20a9c5c6fa6c70d0e
Gergely Risko <errge@nilcons.com>
2021-03-01 15:42:34 +0100

    ledger emulator: abstract the seed from the Dockerfile

--- emulators/ledger-nano-s/02-get-addresses.sh
+++ emulators/ledger-nano-s/02-get-addresses.sh
@@ -2,8 +2,9 @@
 
 set -e
 
+SEED="nation grab van ride cloth wash endless gorilla speed core dry shop raise later wedding sweet minimum rifle market inside have ill true analyst"
 for c in btc eth xrp xlm ltc xtz; do
-    docker run -it --rm speculos-ledger-cli $c \
+    docker run -it --rm speculos-ledger-cli "$SEED" $c \
         | grep -v "Failed to initialize libusb" \
         | grep -v "UTXOs" \
         | cut -d: -f2- \
